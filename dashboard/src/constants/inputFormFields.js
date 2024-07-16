6e105f737049296cb221ccc5c67d159f1d999ac3
Crystal ByNet <cristalbynet@gmail.com>
2024-07-16 12:32:10 +0200

    input fields

---
+++ dashboard/src/constants/inputFormFields.js
@@ -0,0 +1,81 @@
+export const inputFormFields = [
+    {
+        name: 'firstName',
+        label: 'First Name',
+        type: "text"
+    },
+    {
+        name: 'lastName',
+        label: 'Last Name',
+        type: "text"
+    },
+    {
+        name: 'email',
+        label: 'Email',
+        type: "email"
+    },
+    {
+        name: 'password',
+        label: 'Password',
+        type: "password"
+    },
+    {
+        name: 'phone',
+        label: 'Contact Number',
+        type: "text"
+    },
+    {
+        name: 'birth',
+        label: 'Date de naissance',
+        type: "date"
+    },
+    {
+        name: 'gender',
+        label: 'genre',
+        type: "text"
+    },
+    {
+        name: 'doctorDepartement',
+        label: 'department',
+        type: "text"
+    },
+    
+]
+
+export const inputFormAppointment = [
+    {
+        name: 'firstName',
+        label: 'Nom',
+        type: "text"
+    },
+    {
+        name: 'lastName',
+        label: 'Prénom',
+        type: "text"
+    },
+    {
+        name: 'email',
+        label: 'Email',
+        type: "email"
+    },
+    {
+        name: 'phone',
+        label: 'Téléphone Mobile',
+        type: "text"
+    },
+    {
+        name: 'birth',
+        label: 'Date de naissance',
+        type: "date"
+    },
+    {
+        name: 'gender',
+        label: 'Gender',
+        type: "text"
+    },
+    {
+        name: 'address',
+        label: 'Adresse',
+        type: "text"
+    },
+]
