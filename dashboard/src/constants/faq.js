dc1f6d621031b92a4b77fd955f75bea6cf84571e
Crystal ByNet <cristalbynet@gmail.com>
2024-07-16 12:31:52 +0200

    question FAQ

---
+++ dashboard/src/constants/faq.js
@@ -0,0 +1,42 @@
+export const faq = [
+    {
+        id: 1,
+        question: 'Comment puis-je prendre un rendez-vous en ligne?',
+        answer: 'Pour prendre un rendez-vous en ligne, connectez-vous à votre compte, sélectionnez "Prendre un rendez-vous", choisissez le médecin et l’horaire souhaité, puis confirmez votre rendez-vous.'
+    },
+    {
+        id: 2,
+        question: 'Comment puis-je annuler ou reprogrammer un rendez-vous?',
+        answer: 'Pour annuler ou reprogrammer un rendez-vous, accédez à votre historique de rendez-vous, sélectionnez le rendez-vous que vous souhaitez modifier et suivez les instructions pour l’annuler ou le reprogrammer.'
+    },
+    {
+        id: 3,
+        question: 'Comment puis-je accéder à mes résultats de laboratoire?',
+        answer: 'Vous pouvez accéder à vos résultats de laboratoire en vous connectant à votre compte, puis en allant dans la section "Mes résultats" où vous trouverez tous vos résultats de tests et analyses.'
+    },
+    {
+        id: 4,
+        question: 'Comment puis-je mettre à jour mes informations personnelles?',
+        answer: 'Pour mettre à jour vos informations personnelles, connectez-vous à votre compte, allez dans la section "Mon profil" et cliquez sur "Modifier" pour mettre à jour vos informations.'
+    },
+    {
+        id: 5,
+        question: 'Comment puis-je ajouter un membre de ma famille à mon compte?',
+        answer: 'Pour ajouter un membre de votre famille à votre compte, allez dans la section "Famille", cliquez sur "Ajouter un membre", puis entrez les informations nécessaires pour l’ajout.'
+    },
+    {
+        id: 6,
+        question: 'Que faire si j’oublie mon mot de passe?',
+        answer: 'Si vous oubliez votre mot de passe, cliquez sur "Mot de passe oublié" sur la page de connexion et suivez les instructions pour réinitialiser votre mot de passe.'
+    },
+    {
+        id: 7,
+        question: 'Comment puis-je contacter le support technique?',
+        answer: 'Pour contacter le support technique, vous pouvez envoyer un message via la section "Support" de l’application ou appeler le numéro de support indiqué sur notre site web.'
+    },
+    {
+        id: 8,
+        question: 'Comment puis-je vérifier la disponibilité des médecins?',
+        answer: 'Pour vérifier la disponibilité des médecins, connectez-vous à votre compte, allez dans la section "Prendre un rendez-vous", choisissez le médecin et vous verrez les créneaux horaires disponibles.'
+    },
+];
