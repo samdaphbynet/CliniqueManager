b8f14acba0e4194dd7bd08eddfa1e6f021471613
Crystal ByNet <cristalbynet@gmail.com>
2024-07-16 12:30:34 +0200

    index for export default

---
+++ dashboard/src/pages/index.js
@@ -0,0 +1,17 @@
+export { default as SidebarMenu } from './global/SidebarMenu';
+export { default as Topbar } from './global/Topbar';
+
+export { default as Appointment } from './info/Appointment';
+export { default as Doctor } from './info/Doctor';
+
+export { default as AddNewDoctor } from './info/AddNewDoctor';
+export { default as Calendars } from './inputs/Calendars';
+export { default as Messages } from './inputs/Messages';
+export { default as FAQ } from './inputs/FAQ';
+
+export { default as Geography } from './charts/Geography';
+export { default as Line } from './charts/Line';
+export { default as Bar } from './charts/Bar';
+export { default as Pie } from './charts/Pie';
+
+export { default as Dashboard } from './Dashboard';
