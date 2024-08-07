3382e1f02cd9685d34ffac4cfb2618f119f20557
Crystal ByNet <cristalbynet@gmail.com>
2024-08-07 13:07:37 +0200

    added services

--- frontend/src/data/doctor.js
+++ frontend/src/data/doctor.js
@@ -1,3 +1,5 @@
+
+
 export const listDoctor = [
   { name: "Dr. Jean Dupont" },
   { name: "Dr. Marie Curie" },
@@ -32,3 +34,26 @@ export const department = [
     { id: 13, name: "Diabétologie" },
     { id: 14, name: "Médecine du Travail " },
   ];
+
+export const services = [
+  { 
+    id: 1, 
+    title: "Service de Médecine Générale", 
+    image: "generale.jpg", 
+    content: "Le service de médecine générale constitue la pierre angulaire de toute clinique de santé, offrant des soins de santé primaires complets. Nos médecins généralistes sont dévoués à fournir des examens de routine, diagnostiquer et traiter les maladies courantes, et orienter les patients vers des spécialistes au besoin. Nous nous engageons à maintenir votre bien-être général et à vous accompagner dans la prévention des maladies." },
+  { 
+    id: 2, 
+    title: "Service de Pédiatrie", 
+    image: "pediatre.webp", 
+    content: "Notre service de pédiatrie est dédié à la santé et au développement de vos enfants, de la naissance jusqu'à l'adolescence. Nos pédiatres offrent des examens de santé réguliers, des vaccinations, et traitent les maladies infantiles. En surveillant de près le développement physique et mental de chaque enfant, nous veillons à ce qu'ils grandissent en bonne santé et épanouis." },
+  { 
+    id: 3, 
+    title: "Service de Gynécologie et Obstétrique", 
+    image: "gyneco-obstetrique.jpg", 
+    content: "Le service de gynécologie et d'obstétrique de notre clinique offre des soins spécialisés pour la santé reproductive des femmes. De la contraception aux soins prénatals et postnatals, en passant par le suivi des grossesses, nos gynécologues et obstétriciens fournissent un soutien complet. Nous nous engageons à assurer la santé et le bien-être de nos patientes à chaque étape de leur vie reproductive." },
+  { 
+    id: 4, 
+    title: "Service de Dermatologie", 
+    image: "Dermatologie.jpg", 
+    content: "Notre service de dermatologie se concentre sur le diagnostic et le traitement des maladies de la peau, des cheveux et des ongles. Que vous ayez besoin de traiter des affections courantes comme l'acné ou l'eczéma, ou de recevoir des soins spécialisés pour des problèmes plus graves comme les cancers de la peau, nos dermatologues expérimentés sont là pour vous aider. Nous offrons également des interventions cosmétiques pour améliorer l'apparence et la santé de votre peau." },
+]
