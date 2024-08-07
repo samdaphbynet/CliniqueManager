81545af1eda18594cdb60bf201646f8dc07f76d8
Crystal ByNet <cristalbynet@gmail.com>
2024-08-07 13:05:59 +0200

    added function to update doctor

--- dashboard/src/pages/info/Doctor.jsx
+++ dashboard/src/pages/info/Doctor.jsx
@@ -1,13 +1,33 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
 import { DataGrid } from "@mui/x-data-grid";
 import { Avatar, Box, IconButton } from "@mui/material";
 import { Header } from "../../components";
 import AddDoctor from "../../components/AddDoctor";
+import ModalEditDoctor from "../info/ModalEditDoctor";
+// for modal
+// import Modal from "@mui/material/Modal";
+// import {
+//   Button,
+//   FormControl,
+//   InputLabel,
+//   MenuItem,
+//   Select,
+//   TextField,
+//   Typography,
+// } from "@mui/material";
+// import { inputFormAppointment } from "../../constants/inputFormFields";
+// import { department } from "../../constants/mockData";
+// import { toast } from "react-toastify";
 
 const Doctor = () => {
   const [doctors, setDoctors] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [idDoctor, setIdDoctor] = useState();
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   // fetch data from api
   useEffect(() => {
@@ -48,11 +68,11 @@ const Doctor = () => {
         <Avatar src={params.value} alt={params.row.firstName} />
       ),
     },
-    { field: "id", headerName: "ID", width: 220, },
-    { field: "firstName", headerName: "First name", width: 150,},
-    { field: "lastName", headerName: "Last name", width: 150,},
-    { field: "email", headerName: "Email", width: 250,},
-    { field: "age", headerName: "Age", headerAlign: "left", width: 220},
+    { field: "id", headerName: "ID", width: 220 },
+    { field: "firstName", headerName: "First name", width: 150 },
+    { field: "lastName", headerName: "Last name", width: 150 },
+    { field: "email", headerName: "Email", width: 250 },
+    { field: "age", headerName: "Age", headerAlign: "left", width: 220 },
     {
       field: "delete",
       headerName: "Delete",
@@ -60,11 +80,45 @@ const Doctor = () => {
       width: 100,
       renderCell: (params) => (
         <IconButton
+          sx={{
+            width: "100%",
+            height: "100%",
+            borderRadius: "0",
+            "&:hover": {
+              backgroundColor: "#FFB8B8",
+            },
+          }}
           aria-label="delete"
           size="large"
           onClick={() => handleDelete(params.row.id)}
         >
-          <DeleteIcon fontSize="inherit" sx={{color: "red"}}/>
+          <DeleteIcon fontSize="inherit" sx={{ color: "red" }} />
+        </IconButton>
+      ),
+    },
+    {
+      field: "edit",
+      headerName: "Edit",
+      headerAlign: "center",
+      width: 100,
+      renderCell: (params) => (
+        <IconButton
+          sx={{
+            width: "100%",
+            height: "100%",
+            borderRadius: "0",
+            "&:hover": {
+              backgroundColor: "#67CFFF",
+            },
+          }}
+          aria-label="edit"
+          size="large"
+          onClick={() => {
+            handleOpen()
+            setIdDoctor(params.row.id)
+          }}
+        >
+          <EditIcon fontSize="inherit" sx={{ color: "green" }} />
         </IconButton>
       ),
     },
@@ -81,19 +135,31 @@ const Doctor = () => {
 
   return (
     <Box m="20px 30px">
-      <Box display="flex" justifyContent="space-between" alignItems="center" m="0 20px">
+      <Box
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+        m="0 20px"
+      >
         <Header title="Médecins" subtitle="Gérer les médecins" />
-        <AddDoctor title="Ajouter un médecin"/>
+        <AddDoctor title="Ajouter un médecin" />
       </Box>
+      <ModalEditDoctor open={open} onClose={handleClose} idDoctor={idDoctor}/>
+      {/* modal */}
+      
+      {/* fin modal  */}
 
-      <Box m="15px 0 0 0" height="70vh" boxShadow="0 0px 20px 0px #A0A0A0"
+      <Box
+        m="15px 0 0 0"
+        height="70vh"
+        boxShadow="0 0px 20px 0px #A0A0A0"
         sx={{
           "& .MuiDataGrid-root": {
             border: "none",
           },
           "& .MuiDataGrid-cell": {
             borderBottom: "1px solid #A0A0A0",
-            color: "black"
+            color: "black",
           },
           "& .name-column--cell": {
             color: "#000000",
