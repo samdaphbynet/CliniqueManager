7775c5a9bcabfd231b9875d2c9c5fed5226a9879
Crystal ByNet <cristalbynet@gmail.com>
2024-08-15 10:59:09 +0200

    update

--- dashboard/src/components/index.js
+++ dashboard/src/components/index.js
@@ -1,7 +1,5 @@
-export { default as GeographyChart } from './GeographyChart';
 export { default as ProgressCircle } from './ProgressCircle';
 export { default as LineChart } from './LineChart';
-export { default as PieChart } from './PieChart';
 export { default as BarChart } from './BarChart';
 export { default as StatBox } from './StatBox';
 export { default as Header } from './Header';
