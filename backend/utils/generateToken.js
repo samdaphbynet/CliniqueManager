f970cc190045dd52cc6f3db7d86b2443b59f76e3
cristalbynet <cristalbynet@gmail.com>
2024-08-17 00:39:26 +0200

    update generate token

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,14 +1,21 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 export const generateTokenAndSetCookie = (user, res) => {
-    const {id, role} = user
-    const token = jwt.sign({id: id}, process.env.JWT_SECRET_KEY, {expiresIn: "15d"});
+  try {
+    const { id, role } = user;
+    const token = jwt.sign({ id: id }, process.env.JWT_SECRET_KEY, {
+      expiresIn: "15d",
+    });
     const tokenName = role === "Admin" ? "AdminToken" : "PatientToken";
     // set cookie
     res.cookie(tokenName, token, {
-        httpOnly: true,
-        maxAge: 15 * 60 * 60 * 24 * 1000,
-        secure: process.env.NODE_ENV === "production"
-    })
+      httpOnly: true,
+      maxAge: 15 * 24 * 60 * 60 * 1000,
+      secure: process.env.NODE_ENV === "production",
+    });
     return token;
-}
+  } catch (error) {
+    console.log("Error in generateTokenAndSetCookie: ", error);
+    throw new Error("Failed to generate token");
+  }
+};
