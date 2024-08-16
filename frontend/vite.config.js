5c110f6f0f7ef6ee63f96cd19ba4a2c5d634cb85
cristalbynet <cristalbynet@gmail.com>
2024-08-16 22:08:14 +0200

    add config server

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+    open: true,
+    proxy: {
+      '/api': {
+        target: 'https://clinique-manager-api.vercel.app',
+        changeOrigin: true,
+      },
+    },
+  }
 })
