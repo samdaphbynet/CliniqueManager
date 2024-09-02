ae9b67e395ac6037199e1fad7edeb5416748a10e
cristalbynet <cristalbynet@gmail.com>
2024-09-02 12:44:58 +0200

    component document parent

---
+++ dashboard/src/pages/document/Document.jsx
@@ -0,0 +1,82 @@
+import React, { useEffect, useState, useContext } from "react";
+import axios from 'axios';
+import {toast} from 'react-toastify';
+import { Box, IconButton, InputBase } from "@mui/material";
+import Header from "../../components/Header";
+import DocumentTable from "./DocumentTable";
+import SearchIcon from "@mui/icons-material/Search";
+import { Context } from '../../index';
+
+const Document = () => {
+
+  const [patients, setPatients] = useState([])
+  const [filteredPatients, setFilteredPatients] = useState([])
+
+  const {baseUrl} = useContext(Context)
+
+  useEffect(() => {
+      // function async fetched all patient
+      const fetchAllPatients = async () => {
+          try {
+            const res = await axios.get(`${baseUrl}/api/v1/user/allpatient`)
+            if (res.status === 200) {
+                setPatients(res.data.patients)
+                setFilteredPatients(res.data.patients)
+            } else {
+                toast("Error fetching patient")
+            }
+          } catch (error) {
+            console.error("Error fetching patient", error)
+            toast("Error fetching patient")
+          }
+      }
+      fetchAllPatients();
+  }, [])
+
+  const handleSearchedPatients = async (event) => {
+    const searchTerm = event.target.value.toLowerCase();
+    const filtred = patients.filter((pat) => pat.firstName.toLowerCase().includes(searchTerm));
+    setFilteredPatients(filtred)
+  }
+
+  //console.log(patient)
+  return (
+    <>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: "space-between",
+          alignItems: "center",
+          backgroundColor: "#f5f5f5",
+          borderRadius: "10px",
+          padding: "20px",
+          marginTop: "100px",
+          marginLeft: "300px",
+          marginRight: "50px",
+          color: "black",
+          boxShadow: "0px 0px 20px rgba(0, 0, 0, 0.1)",
+        }}
+      >
+        <Header
+          title="Document"
+          subtitle="Les documents reçu de patients"
+        />
+        {/* 🟧🟧🟧🟧🟧🟧🟧🟧🟧🟧🟧🟧 SEARCH BAR 🟧🟧🟧🟧🟧🟧🟧🟧🟧🟧🟧🟧 */}
+        <Box display="flex" borderRadius="3px" backgroundColor="#000000">
+          <InputBase 
+            sx={{ ml: 2, flex: 1 }}
+            placeholder="Recherche"
+            onChange={handleSearchedPatients}
+          />
+
+          <IconButton type="button" sx={{ p: 1 }} >
+            <SearchIcon />
+          </IconButton>
+        </Box>
+      </Box>
+      <DocumentTable patient={filteredPatients}/>
+    </>
+  );
+};
+
+export default Document;
