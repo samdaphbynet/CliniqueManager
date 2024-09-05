f9b26feb1947a2df6e9bfe073119e94dfce4a5ad
cristalbynet <cristalbynet@gmail.com>
2024-09-05 13:14:18 +0200

    update uploader

--- backend/middlewares/upload.js
+++ backend/middlewares/upload.js
@@ -6,25 +6,11 @@ const storage = new CloudinaryStorage({
     cloudinary: cloudinary.v2,
     params: {
         folder: "documents",
-        resource_type: "auto", // "raw"
-        // format: async (req, file) => "pdf",
+        resource_type: "raw",
+        format: async (req, file) => "pdf",
         public_id: (req, file) => file.originalname.split(".")[0],
     }
 });
 
-const fileFilter = (req, res, cb) => {
-    const allowedFormats = ["application/pdf", "application/png", "application/jpg", "application/jpeg", "application/webp"]
-
-    if (allowedFormats.includes(allowedFormats.mimetype)) {
-        cb(null, true);
-    } else {
-        cb(new Error("Invalid file format. Only PDF, PNG, JPG, JPEG, and WebP are allowed."), false);
-    }
-}
-
-const upload = multer(
-    { 
-        storage: storage,
-        fileFilter: fileFilter,
-    });
+const upload = multer({ storage: storage });
 export default upload
