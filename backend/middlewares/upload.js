9bb77638495a7435c1f61a67f2f5ccff263e8656
cristalbynet <cristalbynet@gmail.com>
2024-09-05 12:18:46 +0200

    add fileFilter function

--- backend/middlewares/upload.js
+++ backend/middlewares/upload.js
@@ -6,11 +6,25 @@ const storage = new CloudinaryStorage({
     cloudinary: cloudinary.v2,
     params: {
         folder: "documents",
-        resource_type: "raw",
-        format: async (req, file) => "pdf",
+        resource_type: "auto", // "raw"
+        // format: async (req, file) => "pdf",
         public_id: (req, file) => file.originalname.split(".")[0],
     }
 });
 
-const upload = multer({ storage: storage });
+const fileFilter = (req, res, cb) => {
+    const allowedFormats = ["application/pdf", "application/png", "application/jpg", "application/jpeg", "application/webp"]
+
+    if (allowedFormats.includes(allowedFormats.mimetype)) {
+        cb(null, true);
+    } else {
+        cb(new Error("Invalid file format. Only PDF, PNG, JPG, JPEG, and WebP are allowed."), false);
+    }
+}
+
+const upload = multer(
+    { 
+        storage: storage,
+        fileFilter: fileFilter,
+    });
 export default upload
