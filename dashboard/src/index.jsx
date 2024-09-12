cc7ad9770336edfddbe135566c5f02c4e2847a2e
cristalbynet <cristalbynet@gmail.com>
2024-09-12 22:51:56 +0200

    add isCollapsed in context provider

--- dashboard/src/index.jsx
+++ dashboard/src/index.jsx
@@ -8,10 +8,11 @@ export const Context = createContext({isAuthenticated: false})
 const AppWrapper = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(false)
   const [user, setUser] = useState(false)
+  const [isCollapsed, setIsCollapsed] = useState(false);
   const baseUrl = process.env.REACT_APP_BACKEND_API_VERCEL
 
   return (
-    <Context.Provider value={{isAuthenticated, setIsAuthenticated, user, setUser, baseUrl}}>
+    <Context.Provider value={{isAuthenticated, setIsAuthenticated, user, setUser, baseUrl, isCollapsed, setIsCollapsed}}>
       <App />
     </Context.Provider>
   )
