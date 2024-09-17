cd9294f4726febb9088bff06480385d144a49791
cristalbynet <cristalbynet@gmail.com>
2024-09-17 10:42:55 +0200

    organised form message

--- frontend/src/components/main/MessageForm.jsx
+++ frontend/src/components/main/MessageForm.jsx
@@ -51,75 +51,79 @@ const MessageForm = () => {
 
   return (
     <div id="message" className="message_container">
-        <h1>Envoyer un message</h1>
-      <div className="message">
-        <div className="user_login">
-          <form className="form_message" onSubmit={handleSubmit}>
-            <label>Nom de famille</label>
-            <input
-              value={message.firstName}
-              placeholder="Ex: Patric"
-              onChange={(e) => {
-                setMessage({ ...message, firstName: e.target.value });
-              }}
-              type="text"
-              className="input"
-            />
+      <h1 className="titleMessage">Envoyer un message</h1>
+      <div className="formAndImg">
+        <div className="backgroundLeft">
+        </div>
+        <div className="message">
+          <div className="user_login">
+            <form className="form_message" onSubmit={handleSubmit}>
+              <label>Nom de famille</label>
+              <input
+                value={message.firstName}
+                placeholder="Ex: Patric"
+                onChange={(e) => {
+                  setMessage({ ...message, firstName: e.target.value });
+                }}
+                type="text"
+                className="input"
+              />
 
-            <label>Nom</label>
-            <input
-              value={message.lastName}
-              placeholder="Ex: Adrien"
-              onChange={(e) => {
-                setMessage({ ...message, lastName: e.target.value });
-              }}
-              type="text"
-              className="input"
-            />
+              <label>Nom</label>
+              <input
+                value={message.lastName}
+                placeholder="Ex: Adrien"
+                onChange={(e) => {
+                  setMessage({ ...message, lastName: e.target.value });
+                }}
+                type="text"
+                className="input"
+              />
 
-            <label>Votre Email</label>
-            <input
-              value={message.email}
-              placeholder="Ex: example@example.com"
-              onChange={(e) => {
-                setMessage({ ...message, email: e.target.value });
-              }}
-              type="text"
-              className="input"
-            />
+              <label>Votre Email</label>
+              <input
+                value={message.email}
+                placeholder="Ex: example@example.com"
+                onChange={(e) => {
+                  setMessage({ ...message, email: e.target.value });
+                }}
+                type="text"
+                className="input"
+              />
 
-            <label>Téléphone</label>
-            <input
-              value={message.phone}
-              placeholder="Ex: 0658648158"
-              onChange={(e) => {
-                setMessage({ ...message, phone: e.target.value });
-              }}
-              type="number"
-              className="input"
-            />
-            <label>Message</label>
-            <textarea
-              value={message.message}
-              placeholder="Votre message"
-              onChange={(e) => {
-                setMessage({...message, message: e.target.value });
-              }}
-              className="form-control"
-              rows={7}
-            />
+              <label>Téléphone</label>
+              <input
+                value={message.phone}
+                placeholder="Ex: 0658648158"
+                onChange={(e) => {
+                  setMessage({ ...message, phone: e.target.value });
+                }}
+                type="number"
+                className="input"
+              />
+              <label>Message</label>
+              <textarea
+                value={message.message}
+                placeholder="Votre message"
+                onChange={(e) => {
+                  setMessage({...message, message: e.target.value });
+                }}
+                className="form-control"
+                rows={7}
+              />
 
-            <div className="action_btns">
-              <div className="one_half last">
-                <button
-                  type="submit"
-                  className="btn btn-primary px-4 py-2 m-2 text-center"
-                >
-                  Send message
-                </button>
+              <div className="action_btns">
+                <div className="one_half last">
+                  <button
+                    type="submit"
+                    className="btn btn-primary px-4 py-2 m-2 text-center"
+                  >
+                    Send message
+                  </button>
+                </div>
               </div>
-            </div>
-          </form>
+            </form>
+          </div>
         </div>
       </div>
     </div>
