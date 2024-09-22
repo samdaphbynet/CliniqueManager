379353ac3604b9155dd2f82e2e9557c52dbdabc3
cristalbynet <cristalbynet@gmail.com>
2024-09-22 20:43:30 +0200

    update footer

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -1,62 +1,41 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
 const Footer = () => {
   return (
-    <footer>
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-6">
-            <div className="footer-widget">
-              <h4>Contactez le center clinique</h4>
-              <p>53 Rue toulouse lautrec 31874, <br></br>Toulouse France</p>
-              <p>
-                <a href="#">+33-588-64-84-66</a>
-              </p>
-              <p>
-                <a href="#">clinique@cliniuqe.com</a>
-              </p>
-            </div>
-          </div>
-          <div className="col-lg-3">
-            <div className="footer-widget">
-              <h4>About Us</h4>
-              <ul>
-                <li>
-                  <a href="#">Home</a>
-                </li>
-                <li>
-                  <a href="#">Services</a>
-                </li>
-                <li>
-                  <a href="#">About</a>
-                </li>
-                <li>
-                  <a href="/appointment">Appointment</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div className="col-lg-3">
-            <div className="footer-widget">
-              <h4>Notre Clinique</h4>
-              <div className="logo">
-                <img src="logo1.png" alt="" />
-              </div>
-              <p>
-              Optimisez vos opérations cliniques avec notre plateforme intuitive et sécurisée. Gérez les dossiers des patients, planifiez les rendez-vous, et suivez les traitements avec facilité. Améliorez la qualité des soins et l'efficacité administrative dès aujourd'hui.
-              </p>
-            </div>
-          </div>
-          <div className="col-lg-12">
-            <div className="copyright-text">
-              <p>
-                Copyright © 2024 Clinique de santé. All Rights Reserved.
-                <br />
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
+    <footer className="bg-dark text-light">
+      <Container>
+        <Row>
+          <Col md={3}>
+            <img src="public/logo1.png" alt="" />
+          </Col>
+          <Col md={3}>
+            <h5>ClinicPlus</h5>
+            <p className="text-light">Votre santé, notre priorité. Nous sommes là pour répondre à vos besoins médicaux avec professionnalisme et bienveillance.</p>
+          </Col>
+          <Col md={3}>
+            <h5>Liens utiles</h5>
+            <ul className="list-unstyled">
+              <li><a href="/about" className="text-light">À propos</a></li>
+              <li><a href="/contact" className="text-light">Contactez-nous</a></li>
+              <li><a href="/services" className="text-light">Nos services</a></li>
+              <li><a href="/faq" className="text-light">FAQ</a></li>
+            </ul>
+          </Col>
+          <Col md={3}>
+            <h5>Contact</h5>
+            <p className="text-light">Adresse : 123 Rue de la Santé, 31000 Toulouse</p>
+            <p className="text-light">Téléphone : +33 5 67 89 00 11</p>
+            <p className="text-light">Email : info@clinicplus.com</p>
+          </Col>
+        </Row>
+        <hr className="bg-light"/>
+        <Row>
+          <Col className="text-center">
+            <p className="mb-0 text-light">&copy; {new Date().getFullYear()} ClinicPlus. Tous droits réservés.</p>
+          </Col>
+        </Row>
+      </Container>
     </footer>
   );
 };
